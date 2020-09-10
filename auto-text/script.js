f2e544c0aba0091f9ca7753481288fd1e5968dbd
cocoxiang123 <cocoxiang123@users.noreply.github.com>
2020-09-10 19:27:18 +1000

    init projects

---
+++ auto-text/script.js
@@ -0,0 +1,12 @@
+const text = "Helloooo! Thanks for visiting my website! ";
+
+let index = 0;
+
+function writeText() {
+  document.body.innerText = text.slice(0, index);
+  index++;
+  if (index > text.length - 1) {
+    index = 0;
+  }
+}
+setInterval(writeText, 150);
